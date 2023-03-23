e1aac8a5f6e9a9ad422f2d0b54ff87e3ba69f446
Ben Kirchner <ben@mento.co>
2023-03-23 01:03:04 -0600

    Fix

--- apps/web/pages/booking/[uid]/embed.tsx
+++ apps/web/pages/booking/[uid]/embed.tsx
@@ -4,4 +4,6 @@ import { getServerSideProps as _getServerSideProps } from "../[uid]";
 
 export { default } from "../[uid]";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export const getServerSideProps = withEmbedSsr(_getServerSideProps);
